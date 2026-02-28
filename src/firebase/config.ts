5c50a59252ecb33c8fd9caea61e7f67f69c79a07
ANTA MASSORA <antamassora@gmail.com>
2026-02-28 01:13:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8887998759-466e2",
+  "appId": "1:45182990526:web:aa169013e1ca140d91c6ef",
+  "apiKey": "AIzaSyCIraFb87DunilvZJzFJyZvwohErcgNr88",
+  "authDomain": "studio-8887998759-466e2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "45182990526"
+};
